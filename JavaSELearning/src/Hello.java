570abdbe9f7a00ec06d5d1a2843f9bafa43412d7
dongxiaoyuu <2387230197@qq.com>
2022-01-24 11:02:03 +0800

    Hello world

---
+++ JavaSELearning/src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User:dxy
+ * Date:2022-01-24
+ * Time:10:59
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
