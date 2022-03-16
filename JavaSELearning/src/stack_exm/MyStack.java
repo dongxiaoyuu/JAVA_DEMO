a73592e3ee749fafe2b5cd164f3febcc7343edb2
dongxiaoyuu <2387230197@qq.com>
2022-03-16 19:58:36 +0800

    update

---
+++ JavaSELearning/src/stack_exm/MyStack.java
@@ -0,0 +1,24 @@
+package stack_exm;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User:dxy
+ * Date:2022-03-16
+ * Time:19:46
+ */
+public class MyStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        int ret=stack.peek();//获取栈顶元素
+        System.out.println(ret);
+
+
+    }
+}
